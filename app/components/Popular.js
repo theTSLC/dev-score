c78313dc0a1bd0061cca9a7262e1b9368aed22e0
Tom Le Coney <tleconey@gmail.com>
2019-02-22 17:47:29 -0600

    v1: add State to Popular component

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,17 +1,24 @@
 var React   = require('react');
 
 class Popular extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedLanguage: 'All' // defaults to All on component mount
+    };
+  }
+  
+  updateLanguage (language) {
+    this.setState(function () {
+      return {
+        selectedLanguage: language
+      }
+    });
+  }
+  
   render() {
-
-    constructor(props) {
-      super(props);
-      this.state = {
-        selectedLanguage = 'All'; // defaults to All on component mount
-      };
-    }
-      
     var languages = ['All', 'JavaScript', 'Python', 'Go', 'R', 'HTML', 'CSS'];
-
     return (
       <ul className='languages'>
         {languages.map(function (language) {
