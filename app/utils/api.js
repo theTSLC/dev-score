72d52994420d11cfd0db3e2132534d6204d17809
Tom Le Coney <tleconey@gmail.com>
2019-02-28 15:40:30 -0600

    v1: typo in api call

--- app/utils/api.js
+++ app/utils/api.js
@@ -3,7 +3,7 @@ var axios = require('axios');
 module.exports = {
 
   fetchPopularRepos: function (language) {
-    var encodedURI = window.encodedURI('https://api.github.com/search/repositories?q=stars:>1+language:' + language + '&sort=star&order=desc&type=Repositories');
+    var encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:' + language + '&sort=star&order=desc&type=Repositories');
 
     return axios.get(encodedURI)
       .then(function (response) {
