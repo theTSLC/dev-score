fd40cb90170726263c0e49de7770eef39b342ba0
Tom Le Coney <tleconey@gmail.com>
2019-02-28 15:11:43 -0600

    v1: github api call util

---
+++ app/utils/api.js
@@ -0,0 +1,14 @@
+var axios = require('axios');
+
+module.exports = {
+
+  fetchPopularRepos: function (language) {
+    var encodedURI = window.encodedURI('https://api.github.com/search/repositories?q=stars:>1+language:' + language + '&sort=star&order=desc&type=Repositories');
+
+    return axios.get(encodedURI)
+      .then(function (response) {
+        return response.data.items;
+      });
+  } // end fetchPopularRepos
+
+}
