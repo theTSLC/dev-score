ec8c8e980cdca38750005ff331747d75edeeffa2
Tom Le Coney <tleconey@gmail.com>
2019-02-28 17:33:10 -0600

    v1: style changes

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -93,7 +93,7 @@ class Popular extends React.Component {
           onSelect = {this.updateLanguage} 
         />
         {!this.state.repos 
-          ? <p className='loading-text'>LOADING...</p>
+          ? <p className='loading-text'>Talking to github...</p>
           : <RepoGrid repos={this.state.repos} />
         }
       </div>
