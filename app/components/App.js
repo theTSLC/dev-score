fdb6171103141f2f8a87d0e764b92d60547ed950
Tom Le Coney <tleconey@gmail.com>
2019-02-28 17:11:42 -0600

    v1: adding initial routing

--- app/components/App.js
+++ app/components/App.js
@@ -1,12 +1,19 @@
-var React   = require('react');
+var React       = require('react');
+var ReactRouter = require('react-router-dom');
+var Router      = ReactRouter.BrowserRouter;
+var Router      = ReactRouter.Route;
+
 var Popular = require('./Popular');
+
 
 class App extends React.Component {
   render () {
     return (
-      <div className='container'>
-          <Popular />
-      </div>
+      <Router>
+        <div className='container'>
+          <Route path='/popular' component={Popular} />
+        </div>
+      </Router>
     )
   }
 }
