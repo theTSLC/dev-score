8bc800e2460c2e23b964d2efbe7a437684b8f37b
Tom Le Coney <tleconey@gmail.com>
2019-03-03 16:59:34 -0600

    v1: adding state and update method to Battle

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,6 +1,28 @@
 var React = require('react');
 
 class Battle extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      player1Name : '',
+      player1Image: null,
+      player2Name : '',
+      payer2Image : null,
+    }
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(id, username) {
+    this.setState(function () {
+      var newState = {};
+      newState[id + 'Name'] = username;
+      newState[id + 'Image'] = 'https://github.com/' + username + '.pgn?size=200';
+      return newState;
+    });
+  }
+
   render() {
     return (
       <div>
