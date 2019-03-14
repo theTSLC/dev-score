b2fbc872f413f2eaee62a6da221410dd448b106a
Tom Le Coney <tleconey@gmail.com>
2019-03-13 21:16:41 -0500

    v2: es6 refactor Results component

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,24 +1,24 @@
-var React       = require('react');
-var PropTypes   = require('prop-types');
-var queryString = require('query-string');
-var Link        = require('react-router-dom').Link;
-var api         = require('../utils/api');
+const React       = require('react');
+const PropTypes   = require('prop-types');
+const queryString = require('query-string');
+const Link        = require('react-router-dom').Link;
+const api         = require('../utils/api');
 
-var PlayerPreview = require('./PlayerPreview');
-var Loading       = require('./Loading');
+const PlayerPreview = require('./PlayerPreview');
+const Loading       = require('./Loading');
 
-function Profile (props) { //non reused stateless functional component so no new file needed
-  var info = props.info;
+function Profile ({ info }) { //non reused stateless functional component so no new file needed
+  const { avatar_url, login, name, location, company, followers, following, public_repos, blog } = info;
   return (
-    <PlayerPreview avatar={info.avatar_url} username={info.login}>
+    <PlayerPreview avatar={avatar_url} username={login}>
       <ul className='space-list-items'>
-        {info.name     && <li>{info.name}</li>}
-        {info.location && <li>{info.location}</li>}
-        {info.company  && <li>{info.company}</li>}
-        <li>Followers:    {info.followers}</li>
-        <li>Following:    {info.following}</li>
-        <li>Public Repos: {info.public_repos}</li>
-        {info.blog && <li><a href={info.blog}>{info.blog}</a></li>}
+        {name     && <li>{name}</li>}
+        {location && <li>{location}</li>}
+        {company  && <li>{company}</li>}
+        <li>Followers:    {followers}</li>
+        <li>Following:    {following}</li>
+        <li>Public Repos: {public_repos}</li>
+        {blog && <li><a href={blog}>{blog}</a></li>}
       </ul>
     </PlayerPreview>
   )
@@ -27,12 +27,12 @@ Profile.propTypes = {
   info: PropTypes.object.isRequired
 }
 
-function Player (props) { //non reused stateless functional component so no new file needed
+function Player ({ label, score, profile }) { //non reused stateless functional component so no new file needed
   return (
     <div>
-      <h1 className='header'>{props.label}</h1>
-      <h3 style={{textAlign: 'center'}}>Score: {props.score}</h3>
-      <Profile info={props.profile}/>
+      <h1 className='header'>{label}</h1>
+      <h3 style={{textAlign: 'center'}}>Score: {score}</h3>
+      <Profile info={profile}/>
     </div>
   )
 }
@@ -54,39 +54,30 @@ class Results extends React.Component {
   }
 
   componentDidMount() {
-    var players = queryString.parse(this.props.location.search);
+    const { player1Name, player2Name } = queryString.parse(this.props.location.search);
 
     api.battle([
-      players.player1Name,
-      players.player2Name
-    ])
-      .then(function (results) {
+      player1Name,
+      player2Name
+    ]).then((results) => {
+      if (results === null) {
+        return this.setState(() => ({
+          error : 'Looks like something went wrong. Check that both users exist on Github',
+          loading: false
+        }));
+      }
 
-        if (results === null) {
-          return this.setState(function() {
-            return {
-              error : 'Looks like something went wrong. Check that both users exist on Github',
-              loading: false
-            };
-          });
-        }
-
-        this.setState(function () {
-          return {
-            error: null,
-            winner: results[0],
-            loser: results[1],
-            loading: false
-          }
-        })
-      }.bind(this));
+      this.setState(() => ({
+        error: null,
+        winner: results[0],
+        loser: results[1],
+        loading: false
+      }));
+    });
   }
   
   render() {
-    var error  = this.state.error;
-    var winner = this.state.winner;
-    var loser  = this.state.loser;
-    var loading= this.state.loading;
+    const { error, winner, loser, loading } = this.state;
 
     if (loading === true) {
       return <Loading />
